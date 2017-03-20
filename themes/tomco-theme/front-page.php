a3c0847e927e7571f3862640e7797d4a9864f11f
hollythedev <me@hollythedev.tech>
2017-03-19 21:33:24 -0700

    setting frame for front pagea nd set up coutner

--- themes/tomco-theme/front-page.php
+++ themes/tomco-theme/front-page.php
@@ -6,12 +6,62 @@
  */
 get_header(); ?>
     
-    <div>   
+    <div class= "carousel">   
     
     
     testing
     
     </div>
+
+    <div class="information-counter">
+        <h4 class="counter-title"> We can offer:</h4>
+        <div class="counter-1"
+            <h1 class= "count">31</h1>
+            <h5>years of experience</h5>
+        </div>
+         <div class="counter-2"
+            <h1 class= "count">250</h1>
+            <h5>companies to chose from</h5>
+        </div>
+         <div class="counter-3"
+            <h1 class= "count">32</h1>
+            <h5>shipping ports worldwide</h5>
+        </div>
+    </div>
+
+    <div class= "contact-banner">
+        <h4 class= "banner-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris  eget eros malesuada lacus cursus blandit eget a est. Mauris eget lorem ipsum.</h4>
+        <a class= "banner-link" href= # > Contact us <a>
+    </div>
+
+
+    <div class= "product-preview">
+
+    </div>
+
+    <div class= "skillset-overview">
+        <div class= "skillset1"> 
+        <h5>shipment consolidation</h5>
+        <p>Phasellus in facilisis ligula, a dictum tortor. Morbi et quam ut neque luctus eleifend.</p>
+        </div>
+        <div class= "skillset2"> 
+        <h5>reliable quality</h5>
+        <p>Phasellus in facilisis ligula, a dictum tortor. Morbi et quam ut neque luctus eleifend.</p>
+        </div>
+        <div class= "skillset3"> 
+        <h5>efficient service</h5>
+        <p>Phasellus in facilisis ligula, a dictum tortor. Morbi et quam ut neque luctus eleifend.</p>
+        </div>
+        <div class= "skillset4"> 
+        <h5>large networks</h5>
+        <p>Phasellus in facilisis ligula, a dictum tortor. Morbi et quam ut neque luctus eleifend.</p>
+        </div>
+        <div class= "skillset5"> 
+        <h5>reliable quality</h5>
+        <p>Phasellus in facilisis ligula, a dictum tortor. Morbi et quam ut neque luctus eleifend.</p>
+        </div>
+    </div>
+
     <?php wp_footer(); ?>
         </body>
      
--- themes/tomco-theme/js/scripts.js
+++ themes/tomco-theme/js/scripts.js
@@ -7,3 +7,15 @@ jQuery(function(){
     menuWrap.toggleClass('menu-show');
   });
 })
+
+jQuery('.count').each(function(){
+  jQuery(this).prop('Counter',0).animate({
+    Counter: jQuery(this).text()
+  },{
+    duration:4000,
+    easing:'swing',
+    step: function(now){
+      jQuery(this).text(Math.ceil(now));
+    }
+  })
+})
