9a6f31352455e13013027821cabf890ebe8a9b03
hollythedev <me@hollythedev.tech>
2017-03-19 22:11:21 -0700

    added fonts to stylesheet

--- themes/tomco-theme/front-page.php
+++ themes/tomco-theme/front-page.php
@@ -15,16 +15,16 @@ get_header(); ?>
 
     <div class="information-counter">
         <h4 class="counter-title"> We can offer:</h4>
-        <div class="counter-1"
-            <span class= "count">31</span>
+        <div class="counter-1">
+           <h2 class="count"> 31</span>
             <h5>years of experience</h5>
         </div>
-         <div class="counter-2"
-            <span class= "count">250</span>
+         <div class="counter-2">
+            <h2 class= "count">250</h2>
             <h5>companies to chose from</h5>
         </div>
-         <div class="counter-3"
-            <span class= "count">32</span>
+         <div class="counter-3">
+            <h2 class= "count">32</h2>
             <h5>shipping ports worldwide</h5>
         </div>
     </div>
@@ -32,11 +32,6 @@ get_header(); ?>
     <div class= "contact-banner">
         <h4 class= "banner-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris  eget eros malesuada lacus cursus blandit eget a est. Mauris eget lorem ipsum.</h4>
         <a class= "banner-link" href= # > Contact us <a>
-    </div>
-
-
-    <div class= "product-preview">
-
     </div>
 
     <div class= "skillset-overview">
@@ -62,6 +57,9 @@ get_header(); ?>
         </div>
     </div>
 
+    <div class= "product-preview">
+
+    </div>
     <?php wp_footer(); ?>
         </body>
      
